8c156d37fe583ce85a5e9cb4e0729fa9c0b2705b
Seth Mitchell <sayotte.mitchell@gmail.com>
2021-04-22 09:31:04 -0400

    Materializer Updates

---
+++ src/components/PastListItems.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_LAUNCH_DATA } from "../queries/getLaunchData";
+
+const PastListItems = () => {
+  return (
+    <div>
+      <p>List Item</p>
+    </div>
+  );
+};
+
+export default PastListItems;
